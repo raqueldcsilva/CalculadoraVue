cca3949132ac81b51485a4859b5a387ecbf0ef42
Raqueldcsilva <raquelshdy@gmail.com>
2022-01-03 10:28:54 -0300

    criação dos componentes

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="button"
+    :class="{ double, triple, operation }">
+        0
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {},
+        operation: { type: Boolean },
+        double: { type: Boolean },
+        triple: { type: Boolean }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
